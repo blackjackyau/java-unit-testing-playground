b034e0a9240bf1da0375aa61a6841a248cb4e3fa
blackjackyau <yau.yik.shiung@gmail.com>
2021-05-30 12:44:47 +0800

    disable java dns cache

--- src/main/java/com/blax/unit/testing/playground/UnitTestingPlaygroundApplication.java
+++ src/main/java/com/blax/unit/testing/playground/UnitTestingPlaygroundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnitTestingPlaygroundApplication {
 
 	public static void main(String[] args) {
+		java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
 		SpringApplication.run(UnitTestingPlaygroundApplication.class, args);
 	}
 
